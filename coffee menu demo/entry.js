270ad4bf7aa15a13d5c10cf90c133e039ff1c5be
krijanmachamasi777 <krijanmachamasi37@gmail.com>
2025-02-19 13:32:33 +0545

    first commit

---
+++ coffee menu demo/entry.js
@@ -0,0 +1,9 @@
+function confirmorder() {
+    const confirmation = confirm("Are you sure you want to place this order?");
+    if (confirmation) {
+      // Allow the form submission
+      
+      alert("Order Placed !");
+    }
+  }
+  
